306654970b599b527f389c8e654924d253acd09f
baitou <2184129070@qq.com>
2025-10-22 13:57:24 +0800

    1

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -138,6 +138,13 @@ public class Model extends Observable {
      * */
     public static boolean emptySpaceExists(Board b) {
         // TODO: Fill in this function.
+        for (int c = 0; c < 4; c++){
+            for(int r = 0; r < 4; r++){
+                if (b.tile(c,r) == null) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
@@ -159,6 +166,12 @@ public class Model extends Observable {
      */
     public static boolean atLeastOneMoveExists(Board b) {
         // TODO: Fill in this function.
+        for (int c = 0; c < 4; c++){
+            for(int r = 0; r < 4; r++){
+                if (b.tile(c,r) == null) {
+                    return true;
+                }
+            }
         return false;
     }
 
