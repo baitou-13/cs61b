0523035dc9b9811835c226c8ea1df7be30d1cff0
baitou <2184129070@qq.com>
2025-10-28 14:01:13 +0800

    SLLists

--- proj0/game2048/Main.java
+++ proj0/game2048/Main.java
@@ -11,6 +11,7 @@ public class Main {
 
     /** Number of squares on the side of a board. */
     static final int BOARD_SIZE = 4;
+
     /** Probability of choosing 2 as random tile (as opposed to 4). */
     static final double TILE2_PROBABILITY = 0.9;
 
