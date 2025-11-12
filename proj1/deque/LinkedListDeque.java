fff6b52e5f7b42213ed4c6de1957f32ef732836f
baitou <2184129070@qq.com>
2025-11-12 19:50:05 +0800

    LinkedListDeque
    Two Sentienal Version
    
    In addition, you also need to implement:
    
    public T getRecursive(int index): Same as get, but uses recursion.
    
    todo: circle version

---
+++ proj1/deque/LinkedListDeque.java
@@ -0,0 +1,103 @@
+package deque;
+
+public class LinkedListDeque<Item> {
+
+    //inner class, don't directly use it
+    private class Node {
+        public Item data;
+        public Node next;
+        public Node last;
+
+        public Node(Item data, Node last, Node next) {
+            this.data = data;
+            this.last = last;
+            this.next = next;
+
+        }
+
+        private Item remove() {
+            if (size == 0) {
+                return null;
+            }
+            this.last.next = this.next;
+            this.next.last = this.last;
+            size--;
+            return this.data;
+        }
+    }
+
+    //sentinel
+    private Node head;
+    private Node tail;
+    private int size;
+
+    /* Creates an empty LinkedListDeque */
+    public LinkedListDeque() {
+        head = new Node(null, null, null);
+        tail = new Node(null, null, null);
+        head.next = tail;
+        tail.last = head;
+        size = 0;
+    }
+
+    public LinkedListDeque(Item x) {
+        head = new Node(null, null, null);
+        tail = new Node(null, null, null);
+        Node newNode = new Node(x, head, tail);
+        head.next = newNode;
+        tail.last = newNode;
+        size = 1;
+    }
+
+    public void addFirst(Item x) {
+        Node newNode = new Node(x, head, head.next);
+        head.next.last = newNode;
+        head.next = newNode;
+        size++;
+    }
+
+    public void addLast(Item x) {
+        Node newNode = new Node(x, tail.last, tail);
+        tail.last.next = newNode;
+        tail.last = newNode;
+        size++;
+    }
+
+    public Item removeFirst() {
+        return head.next.remove();
+    }
+
+    public Item removeLast() {
+        return tail.last.remove();
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public Item get(int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        Node current = head.next;
+        while (index > 0) {
+            current = current.next;
+            index--;
+        }
+        return current.data;
+    }
+
+    public void printDeque(){
+        Node current = head.next;
+        while (current != tail){
+            System.out.print(' ' + current.data.toString());
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+}
