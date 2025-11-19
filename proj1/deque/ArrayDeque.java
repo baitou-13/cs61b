23a893dda10032b96a9b98dd878f0c771020d9f7
baitou <2184129070@qq.com>
2025-11-19 17:14:15 +0800

    #

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -5,7 +5,7 @@ import java.util.Iterator;
 public class ArrayDeque<T> implements Deque<T> {
     private Object[] items;
     private static final int DEFAULT_CAPACITY = 8;
-    private int capacity =  DEFAULT_CAPACITY;
+    private int capacity;
 
     //index
     //tail_index - head_index + 1 = size;
@@ -13,9 +13,6 @@ public class ArrayDeque<T> implements Deque<T> {
     private int headIndex;
 
     private int tailIndex() {
-        if (size == 0) {
-            return 0;
-        }
         return (headIndex + size - 1 + capacity) % capacity;
     }
 
@@ -32,21 +29,21 @@ public class ArrayDeque<T> implements Deque<T> {
     }
 
     //Dynamic Size
-    private void ensureSize() {
-        if (size <=  capacity / 4 && capacity > DEFAULT_CAPACITY) { //minimize
-            resize(capacity / 4);
-        } else if (size + 1 > capacity) {//maximize
+    private void ensureSize(int expectedSize) {
+        if (expectedSize > capacity) {
             resize(capacity * 4);
+        } else if (expectedSize <= capacity / 4 && capacity > DEFAULT_CAPACITY) {
+            resize(capacity / 4);
         }
     }
 
     private void resize(int newCapacity) {
-        Object[] temp = items;
-        items = new Object[newCapacity];
+        Object[] newItems = new Object[newCapacity];
         for (int i = 0; i < size; i++) {
-            items[i] = temp[(headIndex + i) % capacity];
+            int newIndex = (headIndex + i) % newCapacity; // 正确计算新索引
+            newItems[newIndex] = items[i];
         }
-
+        items = newItems;
         capacity = newCapacity;
         headIndex = 0;
     }
@@ -63,7 +60,7 @@ public class ArrayDeque<T> implements Deque<T> {
 
     @Override
     public void addFirst(T t) {
-        ensureSize();
+        ensureSize(size+1);
         if (isEmpty()) {
             addLast(t);
             return;
@@ -76,13 +73,8 @@ public class ArrayDeque<T> implements Deque<T> {
 
     @Override
     public void addLast(T t) {
-        ensureSize();
-        if (isEmpty()) {
-            items[headIndex] = t;
-            size++;
-            return;
-        }
-        int index = tailIndex() + 1;
+        ensureSize(size+1);
+        int index = tailIndex();
         items[index] = t;
         size++;
     }
@@ -92,17 +84,24 @@ public class ArrayDeque<T> implements Deque<T> {
         T temp = (T) items[headIndex];
         items[headIndex] = null;
         headIndex = (headIndex + 1) % capacity;
-        ensureSize();
+        ensureSize(size - 1);
         size--;
         return temp;
     }
 
     @Override
     public T removeLast() {
-        T temp = (T) items[tailIndex()];
-        items[tailIndex()] = null;
-        ensureSize();
+        if (isEmpty()) return null;
+        int index = tailIndex();
+        @SuppressWarnings("unchecked")
+        T temp = (T) items[index];
+        items[index] = null;
         size--;
+        if (size == 0) {
+            headIndex = 0; // 如果为空，重置头部索引
+        } else {
+            index = (index + capacity - 1) % capacity; // 正确计算新的尾部索引
+        }
         return temp;
     }
 
